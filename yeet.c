f807327f90aac250f22a9475badde3b0269be864
kunyukzz <aan13prasetya@gmail.com>
2026-05-23 17:51:09 +0800

    target and source

--- yeet.c
+++ yeet.c
@@ -6,5 +6,9 @@ int main(void)
     yeet_compiler_use("CLANG", "C11");
     yeet_compiler_flags("-g", "-Wall", "-Wextra", NULL);
     yeet_compiler_linkers("-lGL", "-lm", NULL);
+
+    yeet_target("lalala", "test_dir/binary", "test_dir/obj");
+    yeet_source_all("test_dir/src");
+
     yeet_build();
 }
--- yeet.h
+++ yeet.h
@@ -1,15 +1,19 @@
 // Your Embedded Executable Tool
 
-#include <ctype.h>  // tolower
-#include <stdarg.h> // va_arg, va_list, va_start, va_end
-#include <stdio.h>  // snprintf
-#include <stdlib.h> // calloc
-#include <string.h> // strncpy
+#include <ctype.h>    // tolower
+#include <stdarg.h>   // va_arg, va_list, va_start, va_end
+#include <stdio.h>    // snprintf
+#include <stdlib.h>   // calloc
+#include <string.h>   // strncpy
+#include <sys/stat.h> // stat, mkdir
 
 #define MAX_FLAGS 64
 #define MAX_FLAG_LEN 32
 #define MAX_LINKERS 16
 #define MAX_LINKER_LEN 16
+
+#define MAX_PATH_LEN 128
+#define MAX_SRC 128
 
 typedef struct {
     char compiler[32];
@@ -21,7 +25,23 @@ typedef struct {
 } compiler_t;
 
 typedef struct {
+    char binary[MAX_PATH_LEN];
+    char object[MAX_PATH_LEN];
+} path_build_t;
+
+typedef struct {
+    char directory[64];
+    char exclude[MAX_PATH_LEN];
+    char storage[MAX_SRC][MAX_PATH_LEN];
+    char *files[MAX_SRC]; // for multi-build in single run
+    int count;
+} source_t;
+
+typedef struct {
     compiler_t compiler_cfg;
+    path_build_t target_path;
+    char target_name[16];
+    source_t sources;
 } yeet_builder;
 
 static yeet_builder *g_yeet = {0};
@@ -32,6 +52,9 @@ void yeet_init(void);
 void yeet_compiler_use(const char *compiler, const char *c_version);
 void yeet_compiler_flags(const char *flag, ...);
 void yeet_compiler_linkers(const char *linker, ...);
+
+void yeet_target(const char *name, const char *binary_path, const char *object_path);
+void yeet_source_all(const char *source_path);
 
 void yeet_build(void);
 
@@ -45,7 +68,15 @@ static inline void _str_lower(char *str)
     }
 }
 
-static inline void _add_flag(const char *flag)
+static inline void _str_copy(char *dest, const char *src, size_t size)
+{
+    if (!dest || !src || size == 0) return;
+
+    strncpy(dest, src, size - 1);
+    dest[size - 1] = '\0';
+}
+
+static void _add_flag(const char *flag)
 {
     if (!g_yeet || !flag) return;
 
@@ -56,12 +87,13 @@ static inline void _add_flag(const char *flag)
     }
 
     int idx = g_yeet->compiler_cfg.flag_count;
-    strncpy(g_yeet->compiler_cfg.flags[idx], flag, MAX_FLAG_LEN - 1);
-    g_yeet->compiler_cfg.flags[idx][MAX_FLAG_LEN - 1] = '\0';
+
+    _str_copy(g_yeet->compiler_cfg.flags[idx], flag, MAX_FLAG_LEN);
+
     g_yeet->compiler_cfg.flag_count++;
 }
 
-static inline void _add_linker(const char *linker)
+static void _add_linker(const char *linker)
 {
     if (!g_yeet || !linker) return;
 
@@ -72,9 +104,38 @@ static inline void _add_linker(const char *linker)
     }
 
     int idx = g_yeet->compiler_cfg.linker_count;
-    strncpy(g_yeet->compiler_cfg.linkers[idx], linker, MAX_LINKER_LEN - 1);
-    g_yeet->compiler_cfg.linkers[idx][MAX_LINKER_LEN - 1] = '\0';
+
+    _str_copy(g_yeet->compiler_cfg.linkers[idx], linker, MAX_LINKER_LEN);
+
     g_yeet->compiler_cfg.linker_count++;
+}
+
+static int _create_dir(const char *path)
+{
+    struct stat st = {0};
+    if (stat(path, &st) == -1) return mkdir(path, 0777);
+    return 0;
+}
+
+static void _set_source(const char *path)
+{
+    if (!path || strcmp(path, ".") == 0 || strcmp(path, "./") == 0)
+    {
+        fprintf(stderr,
+                "(\"%s\") isn't allowed.\n"
+                "Source files must live in a directory (e.g. src/).\n",
+                path ? path : "(null)");
+        exit(1);
+    }
+
+    _str_copy(g_yeet->sources.directory, path, MAX_PATH_LEN);
+
+    for (int i = 0; i < g_yeet->sources.count; ++i)
+    {
+        free(g_yeet->sources.files[i]);
+    }
+
+    g_yeet->sources.count = 0;
 }
 
 /* CORE API IMPLEMENTATION */
@@ -139,6 +200,26 @@ void yeet_compiler_linkers(const char *linker, ...)
     va_end(args);
 }
 
+void yeet_target(const char *name, const char *binary_path, const char *object_path)
+{
+    _str_copy(g_yeet->target_name, name, sizeof(g_yeet->target_name));
+
+    _str_copy(g_yeet->target_path.binary, binary_path, sizeof(g_yeet->target_path.binary));
+    _create_dir(binary_path);
+
+    _str_copy(g_yeet->target_path.object, object_path, sizeof(g_yeet->target_path.object));
+    _create_dir(object_path);
+}
+
+void yeet_source_all(const char *source_path)
+{
+    if (!source_path) return;
+
+    _set_source(source_path);
+
+    g_yeet->sources.exclude[0] = '\0';
+}
+
 // actual building logic
 void yeet_build(void)
 {
@@ -155,6 +236,15 @@ void yeet_build(void)
     for (int i = 0; i < g_yeet->compiler_cfg.linker_count; ++i)
     {
         printf(" [%d] %s\n", i, g_yeet->compiler_cfg.linkers[i]);
+    }
+
+    printf("Name: %s\n", g_yeet->target_name);
+    printf("Binary Path: %s, object Path: %s\n", g_yeet->target_path.binary, g_yeet->target_path.binary);
+
+    printf("Source Path: %s\n", g_yeet->sources.directory);
+    for (int i = 0; i < g_yeet->sources.count; ++i)
+    {
+        printf(" [%d] %s\n", i, *g_yeet->sources.files);
     }
 
     free(g_yeet);
